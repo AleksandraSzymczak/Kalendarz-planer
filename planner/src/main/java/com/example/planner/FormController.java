7398295624772c7327d69ada4874d14a43c472d4
AleksandraSzymczak <aleksis550@wp.pl>
2023-06-23 01:55:35 +0200

    Zawijanie tekstu

--- planner/src/main/java/com/example/planner/FormController.java
+++ planner/src/main/java/com/example/planner/FormController.java
@@ -4,13 +4,14 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
 import java.time.LocalDate;
 
 public class FormController {
     @FXML
-    private TextField opis;
+    private TextArea opis;
     @FXML
     private TextField tytul;
     @FXML
